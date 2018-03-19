f91ea00aa316b5121352cc3bb4106747e62a4758
Houdael <33830670+Houdael@users.noreply.github.com>
2018-03-19 16:01:42 +0100

    Add files via upload

---
+++ fuzzy-fwk/Then.h
@@ -0,0 +1,15 @@
+#ifndef THEN_H
+#define THEN_H
+
+#include BinaryExpression.h
+
+namespace core
+{
+	template <typename T>
+	class Then : public BinaryExpression<T>
+	{
+		public : 
+		virtual ~Then() const = default;
+		virtual T evaluate(Expression<T>*, Expression<T>*) const;
+	}
+}
