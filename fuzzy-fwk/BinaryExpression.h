9af1f046c7c4bf4ad0288adc3fde7f01a8917362
Houdael <33830670+Houdael@users.noreply.github.com>
2018-03-19 16:20:22 +0100

    Update BinaryExpression.h

--- fuzzy-fwk/BinaryExpression.h
+++ fuzzy-fwk/BinaryExpression.h
@@ -1,1 +1,17 @@
+#ifndef BINARYEXPRESSION_H
+#define BINARYEXPRESSION_H
 
+#include "Expression.h"
+
+namespace core 
+{
+    template <typename T>
+      class BinaryExpression
+      {
+      public : 
+      virtual ~BinaryExpression() = default;
+      virtual T evaluate(Expression<T>*, Expression<T>*) const = 0;
+      }
+}
+
+#endif
